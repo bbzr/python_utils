cdae5a05274ae0522dec3e39a951214b82d09707
Vitalii Uslystyi <vu@socprime.com>
2020-02-28 11:33:10 +0200

    feat: added tcp_conn module

---
+++ utils/tcp_conn.py
@@ -0,0 +1,51 @@
+import socket
+import json
+import time
+from utils.logger import get_logger
+
+
+class TcpConnection:
+    def __init__(self, host, port):
+        self.conn_timeout = 10
+        self.wait_time_to_reconnect = 60
+        self.sent_event_number = 0
+        self.logger = get_logger()
+        self.tcp_host = host
+        self.tcp_port = int(port)
+        self.set_tcp_conn()
+
+    def set_tcp_conn(self):
+        self.close_tcp_conn()
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect((self.tcp_host, self.tcp_port))
+            self.sock.settimeout(self.conn_timeout)
+        except Exception as err:
+            self.logger.error('Can not connect to logstash. {}'.format(err))
+            raise Exception
+
+    def close_tcp_conn(self):
+        try:
+            self.sock.close()
+        except:
+            pass
+
+    def send_event(self, event):
+        while True:
+            try:
+                if event:
+                    if isinstance(event, dict):
+                        event = json.dumps(event, default=str)
+                    self.sock.send(event.encode() + b'\n')
+                    self.sent_event_number += 1
+                break
+            except Exception as err:
+                self.logger.warning('Can not send event to logstash. Sleeping {} seconds. {}'.format(self.wait_time_to_reconnect, err))
+                time.sleep(self.wait_time_to_reconnect)
+                try:
+                    self.close_tcp_conn()
+                    self.set_tcp_conn()
+                except:
+                    pass
+                self.send_event(event)
+                break
